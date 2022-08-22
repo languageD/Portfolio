abc81cd2cea58c0dbbbd744b09fa438d34acad8f
languageD <victorgrs11@outlook.com>
2022-08-22 17:26:17 -0300

    making home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import styles from './Home.module.css'
 import bgImage from '../img/bg.jpg'
 
+
 function Home(){
   return(
-    <div className={styles.home}>
-      <h1> Full-Stack developer </h1>
-      <img src={bgImage} alt="background Image" />
+    <div className={styles.img_wrapper}>
+      <img src={bgImage } alt="background" />
+      
     </div>
   )
 }
