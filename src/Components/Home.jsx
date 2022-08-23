2aeec856dcbae23ca68222d2af0797147b1d5e97
languageD <victorgrs11@outlook.com>
2022-08-22 22:48:53 -0300

    ending home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,13 @@ import bgImage from '../img/bg.jpg'
 function Home(){
   return(
     <div className={styles.img_wrapper}>
+      <div className={styles.home_info}>
+        <h1>Full-stack developer & designer  </h1>
+        <p >fgdfgdgdfgdfg</p>
+      </div>
+      
       <img src={bgImage } alt="background" />
+      
       
     </div>
   )
