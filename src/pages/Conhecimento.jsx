251c8c1e965541cac01d0871ddbea1ed892ae12d
languageD <victorgrs11@outlook.com>
2022-09-12 20:19:25 -0300

    starting knowledge page

--- src/pages/Conhecimento.jsx
+++ src/pages/Conhecimento.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from './Conhecimento.module.css'
-import {FaJava} from 'react-icons/fa'
+import {IoLogoJavascript} from 'react-icons/io'
+import {FaNodeJs, FaGithub, FaHtml5, FaCss3} from 'react-icons/fa'
+import {SiTypescript} from 'react-icons/si'
 
 
 function Conhecimento(){
@@ -13,7 +15,28 @@ function Conhecimento(){
 
         <div className={styles.conhecimentos}>
           <div className={styles.conhecimento}>
-            <h2 className={styles.conhecimentoItem}><FaJava></FaJava>Javascript</h2>
+            <h2 className={styles.conhecimentoItem}>
+          <IoLogoJavascript/>Javascript</h2>
+          </div>
+          <div className={styles.conhecimento}>
+            <h2 className={styles.conhecimentoItem}>
+          <SiTypescript/>TypeScript</h2>
+          </div>
+          <div className={styles.conhecimento}>
+            <h2 className={styles.conhecimentoItem}>
+          <FaNodeJs/>NodeJS</h2>
+          </div>
+          <div className={styles.conhecimento}>
+            <h2 className={styles.conhecimentoItem}>
+          <FaGithub/>GitHub</h2>
+          </div>
+          <div className={styles.conhecimento}>
+            <h2 className={styles.conhecimentoItem}>
+          <FaHtml5/>Html</h2>
+          </div>
+          <div className={styles.conhecimento}>
+            <h2 className={styles.conhecimentoItem}>
+          <FaCss3/>Css</h2>
           </div>
         </div>
       </div>
