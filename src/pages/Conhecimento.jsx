b7319a5ccae18fb0bbf31cf157e36ce7abd79318
languageD <victorgrs11@outlook.com>
2022-09-13 19:43:03 -0300

    update

--- src/pages/Conhecimento.jsx
+++ src/pages/Conhecimento.jsx
@@ -7,40 +7,46 @@ import {SiTypescript} from 'react-icons/si'
 
 function Conhecimento(){
   return(
-    <div className={styles.container}> 
+  
       <div className={styles.ConhecimentoSessao}>
         <div className={styles.header}>
           <h1>Conhecimentos</h1>
+          <hr />
         </div>
 
         <div className={styles.conhecimentos}>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <IoLogoJavascript/>Javascript</h2>
+          <p></p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <SiTypescript/>TypeScript</h2>
+          <p className={styles.conhecimento_text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos earum, fugiat sit eaque sequi nisi doloremque quisquam doloribus quidem. Enim soluta quaerat minima dicta? Earum, eaque eligendi! Corporis, consectetur sint?</p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaNodeJs/>NodeJS</h2>
+          <p className={styles.conhecimento_text}></p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaGithub/>GitHub</h2>
+          <p className={styles.conhecimento_text}></p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaHtml5/>Html</h2>
+          <p className={styles.conhecimento_text}></p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaCss3/>Css</h2>
+          <p className={styles.conhecimento_text}></p>
           </div>
         </div>
       </div>
-    </div>
   )
   
 }
