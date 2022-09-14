52b3f1c3e0bc29cf6b0dadb52cd1d68202327a5e
languageD <victorgrs11@outlook.com>
2022-09-14 18:00:02 -0300

    update knowledge

--- src/pages/Conhecimento.jsx
+++ src/pages/Conhecimento.jsx
@@ -3,6 +3,7 @@ import styles from './Conhecimento.module.css'
 import {IoLogoJavascript} from 'react-icons/io'
 import {FaNodeJs, FaGithub,  FaReact} from 'react-icons/fa'
 import {SiTypescript, SiMongodb} from 'react-icons/si'
+import mini_bg from '../img/mini-bg.png'
 
 
 function Conhecimento(){
@@ -18,37 +19,40 @@ function Conhecimento(){
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <IoLogoJavascript/>Javascript</h2>
-          <p className={styles.conhecimento_text}>Minha principal linguagem de programação, onde tenho mais conhecimento e dominio sobre, fazendo diversos projetos com a mesma</p>
+          <p className={styles.conhecimento_text}>Minha principal linguagem de programação, onde tenho mais conhecimento e dominío sobre, fazendo diversos projetos com a mesma.</p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <SiTypescript/> TypeScript</h2>
-          <p className={styles.conhecimento_text}>um otimo apoio para o Javascript, gosto bastante de usa-la pela sua facilidade e utilidade pelo fato de ser  extremamente tipada </p>
+          <p className={styles.conhecimento_text}> Um ótimo apoio para o Javascript, gosto bastante de usa-la pela sua facilidade e utilidade pelo fato de ser extremamente típada.</p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaNodeJs/> NodeJS</h2>
           <p className={styles.conhecimento_text}>
-            Um dos back-ends que mais utilizo por ser Facil de manusear  
-            e conseguir fazer um otimo desempenho em varios sistemas
+          Um dos back-ends que mais utilizo por ser Fácil de manusear
+          e conseguir fazer um ótimo desempenho em vários sistemas.
           </p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaGithub/> GitHub</h2>
-          <p className={styles.conhecimento_text}>Um habiente perfeito para demonstar meus projetos, com diversas ferramentas que ajuda eu e muito outros usuarios  </p>
+          <p className={styles.conhecimento_text}>Um habitante perfeito para demonstrar meus projetos, com diversas ferramentas que ajuda eu e muito outros usuários.  </p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <SiMongodb/> Mongodb</h2>
-          <p className={styles.conhecimento_text}>Sistema de banco de dados que  mais tenho experiencia, de facil usabilidade e extenso reportorio de ferramentas</p>
+          <p className={styles.conhecimento_text}>Sistema de banco de   dados que mais tenho experiencia, de fácil usabilidade e extenso reportório de ferramentas.</p>
           </div>
           <div className={styles.conhecimento}>
             <h2 className={styles.conhecimentoItem}>
           <FaReact/> ReactJS</h2>
           <p className={styles.conhecimento_text}>
-            Front-end extremamente forte e facil de aplica-lo ao mesmo tempo, gosto tanto de usar e estou utilizando o mesmo para fazer esse portfolio.
+            Front-end extremamente forte e fácil de aplica-lo ao mesmo tempo, gosto tanto de usar e estou utilizando o mesmo para fazer esse portfólio.
           </p>
+          </div>
+          <div className={styles.imgbg}>
+            <img src={mini_bg} alt="background photo" />
           </div>
         </div>
       </div>
