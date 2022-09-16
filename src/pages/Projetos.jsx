9971462babe59b5b6f8f9cdb7c457c6733391660
languageD <victorgrs11@outlook.com>
2022-09-16 15:33:50 -0300

    update project page

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -9,16 +9,24 @@ function Projetos(){
         <h1>Projetos</h1>
       </div>
 
-      <div className={styles.projects}>
-        <img src={costsImg} alt="costs image" />
-        <p>Costs é um projeto feito totalmente em reactJS, para ser usado como um planejamento de gastos </p>
-      </div> 
+    
+        <div className={styles.projects}>
+          <div className={styles.cards_img}>
+            <img src={costsImg} alt="costs image" />
+          </div>
+          <h2>Costs</h2>
+          <h3>ReactJS</h3>
+          <p  className={styles.project_txt}>Costs é um projeto feito totalmente em reactJS, para ser usado como um planejamento de gastos </p>
+        </div> 
 
-      <div className={styles.projects}>
-        <img src={costsImg} alt="costs image" />
-        <p>PROJETO FUTURO </p>
+        <div className={styles.projects}>
+          <div className="img_card">
+            <img src={costsImg} alt="costs image" />
+          </div>
+          <p className={styles.project_txt}>PROJETO FUTURO </p>
+        </div>
       </div>
-    </div>
+    
   )
 }
 
