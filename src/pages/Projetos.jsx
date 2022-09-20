1f4adfdc450a2359d5c0fb2a6a51ee55b1fca0dd
languageD <victorgrs11@outlook.com>
2022-09-20 16:07:15 -0300

    project page update

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './Projetos.module.css'
 import costsImg from '../img/savings.svg'
+import Container from '../Components/Container'
 
 function Projetos(){
   return(
@@ -9,23 +10,32 @@ function Projetos(){
         <h1>Projetos</h1>
       </div>
 
-    
-        <div className={styles.projects}>
+      <div className={styles.card_container}>
+        <div className={styles.card}>
           <div className={styles.cards_img}>
             <img src={costsImg} alt="costs image" />
           </div>
-          <h2>Costs</h2>
-          <h3>ReactJS</h3>
-          <p  className={styles.project_txt}>Costs é um projeto feito totalmente em reactJS, para ser usado como um planejamento de gastos </p>
+          <div className={styles.card_info}>
+            <h2>Costs</h2>
+            <h3>ReactJS</h3>
+            <p  className={styles.project_txt}>Costs é um projeto feito totalmente em reactJS, para ser usado como um planejamento de gastos </p>
+          </div>
         </div> 
-
-        <div className={styles.projects}>
-          <div className="img_card">
+        <div className={styles.card}>
+          <div className={styles.cards_img}>
             <img src={costsImg} alt="costs image" />
           </div>
-          <p className={styles.project_txt}>PROJETO FUTURO </p>
-        </div>
+          <div className="card_info">
+            <h2>Costs</h2>
+            <h3>ReactJS</h3>
+            <p  className={styles.project_txt}>Costs é um projeto feito totalmente em reactJS, para ser usado como um planejamento de gastos </p>
+          </div>
+        </div> 
       </div>
+
+
+    
+    </div>
     
   )
 }
