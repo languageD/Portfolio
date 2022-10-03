11096fc0e976648663bee9393eb51f845bb24b52
languageD <victorgrs11@outlook.com>
2022-10-03 20:09:10 -0300

    creating contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Container from '../src/Components/layout/Container'
 import Home from './Components/pages/Home'
 import Projetos from './Components/pages/Projetos'
 import Conhecimento from './Components/pages/Conhecimento'
+import Contatos from './Components/pages/Contatos'
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
           <Route exact path='/' element={<Home/>}></Route> 
           <Route path='/Projetos' element={<Projetos/>}></Route> 
           <Route path='/Conhecimento' element={<Conhecimento/>}></Route> 
-        
+          <Route path='/Contatos' element={<Contatos/>}></Route>
         </Routes>
       </Container>   
     </Router>
---
+++ src/Components/pages/Contatos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contatos(){
+  return <h1>contatos</h1> 
+}
+
+export default Contatos
