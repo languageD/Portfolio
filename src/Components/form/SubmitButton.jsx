aabfea989837ad695f2a5f30910730aba166276c
languageD <victorgrs11@outlook.com>
2022-10-05 18:03:39 -0300

    creating a submit button

---
+++ src/Components/form/SubmitButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./SubmitButton.module.css";
+
+function Submitbutton(text){
+  return(
+    <div>
+      <button className={styles.Container}>{text}</button>
+    </div>
+  )
+  
+}
+
+
+export default Submitbutton
--- src/Components/pages/Contatos.jsx
+++ src/Components/pages/Contatos.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Input from "../form/Input";
 
+
 function Contatos(){
 
   const [email, setemail] = useState('')
