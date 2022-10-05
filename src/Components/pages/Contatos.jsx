54316636a5cbed606119040c015ebf44cd7a5561
languageD <victorgrs11@outlook.com>
2022-10-05 17:51:57 -0300

    getting email box and mesagem box

--- src/Components/pages/Contatos.jsx
+++ src/Components/pages/Contatos.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import input from "../form/input";
+import { useState } from "react";
+import Input from "../form/Input";
 
 function Contatos(){
+
+  const [email, setemail] = useState('')
+
+  function submit(e){
+    e.preventDafault()
+  }
+
+  function sendEmail(e){
+    setemail(
+      e.target.value
+    )
+  } 
+
+  
+  
   return (
     <div> {/* conteiner */}
       <div> {/* div header*/}
         <h1>contatos</h1> 
       </div>
 
-      <div> {/* criar o form*/}
-        
-      </div>
+      <form onSubmit={submit}> {/* criar o form*/}
+        <Input text='E-mail:' type='text' name='email' placeholder='E-mail' handleOnChange={sendEmail}/>
+        <Input text='Mensagem:' type='text' name='Mensagem' placeholder='Mesagem' handleOnChange={sendEmail}/>
+      </form>
 
     </div>
   )  
