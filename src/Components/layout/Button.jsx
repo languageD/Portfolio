677b4cf110a266eded3ea0b072be25ed07b3f42e
victor <84206743+languageD@users.noreply.github.com>
2022-11-20 01:55:51 -0300

    .

--- src/Components/layout/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from './Button.module.css'
-
-function Button({name , to}){
-  return <a href={to} target='_blank'><button className={styles.button}> {name}</button></a>
-}
-
-export default Button
